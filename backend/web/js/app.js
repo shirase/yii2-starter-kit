d2e2f6ae09be9947717978c2d2f5f324c3356298
shirase <shirase55@gmail.com>
2016-03-17 17:09:12 +0300

    app.js search

--- backend/web/js/app.js
+++ backend/web/js/app.js
@@ -10,4 +10,25 @@ $(function() {
         zIndex: 999999
     }).disableSelection();
     $(".connectedSortable .box-header, .connectedSortable .nav-tabs-custom").css("cursor", "move");
+
+    (function() {
+        var pageTitle = $('.content-wrapper .content-header h1:first');
+        if(pageTitle.html()) {
+            $('.content-wrapper .content h1:first').remove();
+        } else {
+            var h1 = $('.content-wrapper .content h1:first');
+            pageTitle.replaceWith(h1);
+        }
+
+        var searchForm = $('.content-wrapper .content .search-form');
+        if(searchForm.length) {
+            searchForm
+                .wrap('<div class="box-body" />')
+                .parent()
+                .wrap('<div class="box collapsed-box" />')
+                .parent()
+                .prependTo($('.content-wrapper .content'))
+                .prepend($('<div class="box-header"><div class="box-title" onclick="$.AdminLTE.boxWidget.collapse($(this).next().find(\'button\'))" style="cursor: pointer">Поиск</div><div class="box-tools pull-right"><button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button></div></div>'));
+        }
+    })();
 })
