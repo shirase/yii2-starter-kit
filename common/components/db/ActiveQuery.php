0cd4b93e462cf3fbccc7d6b0f89c8db1d6e8f4cc
shirase <shirase55@gmail.com>
2016-03-18 17:37:03 +0300

    components/db/ActiveQuery.php DateRange RangeFilter

--- common/components/db/ActiveQuery.php
+++ common/components/db/ActiveQuery.php
@@ -1,5 +1,6 @@
 <?php
 namespace common\components\db;
+use Aws\Common\Enum\DateFormat;
 
 /**
  * Class ActiveQuery
@@ -7,4 +8,45 @@ namespace common\components\db;
  */
 class ActiveQuery extends \yii\db\ActiveQuery {
 
+    public function andFilterRange($column, $range) {
+        if($range) {
+            $m = explode(' - ', $range);
+            if(sizeof($m)==2) {
+                if($m[0] && $m[1]) {
+                    $this->andFilterWhere(['OR', ['>=', $column, $m[0]], ['<=', $column, $m[1]]]);
+                } elseif($m[0]) {
+                    $this->andFilterWhere(['>=', $column, $m[0]]);
+                } elseif($m[1]) {
+                    $this->andFilterWhere(['<=', $column, $m[1]]);
+                }
+            } else {
+                $this->andFilterWhere(['like', $column, $range]);
+            }
+        }
+
+        return $this;
+    }
+
+    public function andDateRange($column, $range) {
+        /* @var $modelClass ActiveRecord */
+        $modelClass = $this->modelClass;
+        $db = $modelClass::getDb();
+
+        if($range) {
+            $m = explode(' - ', $range);
+            if(sizeof($m)==2) {
+                if($m[0] && $m[1]) {
+                    $this->andFilterWhere(['AND', ['>=', 'DATE('.$db->quoteColumnName($column).')', $m[0]], ['<=', 'DATE('.$db->quoteColumnName($column).')', $m[1]]]);
+                } elseif($m[0]) {
+                    $this->andFilterWhere(['>=', 'DATE('.$db->quoteColumnName($column).')', $m[0]]);
+                } elseif($m[1]) {
+                    $this->andFilterWhere(['<=', 'DATE('.$db->quoteColumnName($column).')', $m[1]]);
+                }
+            } else {
+                $this->andFilterWhere(['like', 'DATE('.$column.')', $range]);
+            }
+        }
+
+        return $this;
+    }
 }
