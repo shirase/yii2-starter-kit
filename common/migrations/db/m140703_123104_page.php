963d100a5f11446e49654f394f7e598eb0749472
shirase <shirase55@gmail.com>
2016-05-12 22:50:35 +0300

    common/migrations

--- common/migrations/db/m140703_123104_page.php
+++ common/migrations/db/m140703_123104_page.php
@@ -31,21 +31,6 @@ class m140703_123104_page extends Migration
         $this->createIndex('pos', '{{%page}}', array('pos'), true);
         $this->createIndex('pid_pos', '{{%page}}', array('pid', 'pos'), true);
         $this->createIndex('bpath', '{{%page}}', array('bpath(100)'), true);
-
-        $this->createTable('{{%page_view}}', [
-            'id' => $this->primaryKey(),
-            'name' => $this->string(100)->notNull(),
-            'status' => $this->smallInteger(1)->notNull()->defaultValue(1),
-            'plugin' => $this->string(100),
-        ], $tableOptions);
-        $this->addForeignKey('fk_view', '{{%page}}', 'view_id', '{{%page_view}}', 'id');
-
-        $this->createTable('{{%page_view_link}}', [
-            'id' => $this->primaryKey(),
-            'link' => $this->string(255),
-        ], $tableOptions);
-        $this->addForeignKey('fk_page', '{{%page_view_link}}', 'id', '{{%page}}', 'id');
-        $this->insert('{{%page_view}}', ['name'=>'Ссылка', 'plugin'=>'common\plugins\page_view\link\Plugin']);
     }
 
     public function down()
