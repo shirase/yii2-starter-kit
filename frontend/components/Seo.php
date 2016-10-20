ba53e4ddde0015eb8aace20fde07907691724788
shirase <shirase55@gmail.com>
2016-10-20 17:55:47 +0300

    frontend/components/Seo.php

---
+++ frontend/components/Seo.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace frontend\components;
+
+use yii\db\ActiveRecord;
+use yii\helpers\StringHelper;
+
+class Seo
+{
+    /**
+     * @param $model ActiveRecord
+     */
+    public static function make($model) {
+        $view = \Yii::$app->controller->view;
+
+        if($model->hasAttribute('title') && $model->title) {
+            $view->title = $model->title;
+        }
+        elseif($model->hasAttribute('name') && $model->name) {
+            $view->title = $model->name;
+        }
+
+        if($model->hasAttribute('body') && $model->body) {
+            $view->registerMetaTag(['name' => 'description', 'content'=>StringHelper::truncateWords(strip_tags($model->body), 10, '')], 'description');
+        }
+        elseif($view->title) {
+            $view->registerMetaTag(['name' => 'description', 'content'=>$view->title], 'description');
+        }
+
+        if($view->title) {
+            $view->registerMetaTag(['name' => 'keywords', 'content'=>$view->title], 'keywords');
+        }
+    }
+}
