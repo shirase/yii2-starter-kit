831e4f3f4b23a31f973f2662df63b43a5203e8d5
shirase <shirase55@gmail.com>
2017-01-29 23:31:54 +0300

    common/components/web/Controller.php

--- common/components/web/Controller.php
+++ common/components/web/Controller.php
@@ -6,6 +6,13 @@ namespace common\components\web;
  * Class Controller
  * @package common\components\web
  */
-class Controller extends \yii\web\Controller {
-
+class Controller extends \yii\web\Controller
+{
+    public function render($view, $params = []) {
+        if (\Yii::$app->request->isAjax) {
+            parent::renderAjax($view, $params);
+        } else {
+            parent::render($view, $params);
+        }
+    }
 }
