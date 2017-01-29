db38a910adeec92eb85e30df33dd8d08762a7003
shirase <shirase55@gmail.com>
2017-01-29 22:59:16 +0300

    fix

--- frontend/views/page/view.php
+++ frontend/views/page/view.php
@@ -3,10 +3,10 @@
  * @var $this \yii\web\View
  * @var $model \common\models\Page
  */
-$this->title = $this->title ?: $model->title;
+$this->title = $this->title ?: ($model->title ?: $model->name);
 ?>
 <div class="content">
-    <h1><?php echo $model->title ?></h1>
+    <h1><?php echo $model->title ?: $model->name ?></h1>
     <?php \frontend\widgets\InlineEditor::begin(['model'=>$model, 'attribute'=>'body']) ?>
     <?php echo $model->body ?>
     <?php \frontend\widgets\InlineEditor::end() ?>
