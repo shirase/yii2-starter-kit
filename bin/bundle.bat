d3da274d00a2bab2840df86af9f404ec519fe229
shirase <shirase55@gmail.com>
2017-03-02 12:02:20 +0300

    bin/bundle

---
+++ bin/bundle.bat
@@ -0,0 +1,2 @@
+..\console\yii asset ..\backend\config\assets\compress.php ..\backend\config\assets\_bundles.php
+..\console\yii asset ..\frontend\config\assets\compress.php ..\frontend\config\assets\_bundles.php
---
+++ bin/bundle.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+../console/yii asset ../backend/config/assets/compress.php ../backend/config/assets/_bundles.php
+../console/yii asset ../frontend/config/assets/compress.php ../frontend/config/assets/_bundles.php
