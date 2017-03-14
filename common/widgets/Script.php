803b0bd495390b82f90e8cb8531547091ef28835
shirase <shirase55@gmail.com>
2017-03-14 11:27:02 +0300

    common/widgets/Script.php

---
+++ common/widgets/Script.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace common\widgets;
+
+use yii\base\Widget;
+use yii\web\View;
+
+class Script extends Widget
+{
+    public function init() {
+        ob_start();
+    }
+
+    public function run() {
+        $script = ob_get_clean();
+        $script = preg_replace('#\<script[^>]*\>(.*)\<\/script\>#is', '$1', $script);
+        $this->view->registerJs($script);
+    }
+}
