688c2dbd58438d982a19b52d8a50f0e6cf293124
shirase <shirase55@gmail.com>
2017-03-15 10:42:45 +0300

    frontend page SEO

--- frontend/components/Seo.php
+++ frontend/components/Seo.php
@@ -13,22 +13,28 @@ class Seo
     public static function make($model) {
         $view = \Yii::$app->controller->view;
 
-        if($model->hasAttribute('title') && $model->title) {
+        if($model->hasAttribute('page_title') && $model->page_title) {
+            $view->title = $model->page_title;
+        }
+        elseif($model->hasAttribute('title') && $model->title) {
             $view->title = $model->title;
         }
         elseif($model->hasAttribute('name') && $model->name) {
             $view->title = $model->name;
         }
 
-        if($model->hasAttribute('body') && $model->body) {
+        if($model->hasAttribute('page_description') && $model->page_description) {
+            $view->registerMetaTag(['name' => 'description', 'content'=>$model->page_description], 'description');
+        }
+        elseif($model->hasAttribute('body') && $model->body) {
             $view->registerMetaTag(['name' => 'description', 'content'=>StringHelper::truncateWords(strip_tags($model->body), 10, '')], 'description');
         }
-        elseif($model->hasAttribute('title') && $view->title) {
-            $view->registerMetaTag(['name' => 'description', 'content'=>$view->title], 'description');
+        elseif($model->hasAttribute('text') && $model->text) {
+            $view->registerMetaTag(['name' => 'description', 'content'=>StringHelper::truncateWords(strip_tags($model->text), 10, '')], 'description');
         }
 
-        if($model->hasAttribute('title') && $view->title) {
-            $view->registerMetaTag(['name' => 'keywords', 'content'=>$view->title], 'keywords');
+        if($model->hasAttribute('page_keywords') && $model->page_keywords) {
+            $view->registerMetaTag(['name' => 'keywords', 'content'=>$model->page_keywords], 'keywords');
         }
     }
 }
