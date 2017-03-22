eba0104d92f6c6fc9d2ed9c5e53eb6097606ac1f
shirase <shirase55@gmail.com>
2017-03-22 22:39:13 +0300

    #4 Fix "Controversial/Superglobals" issue in frontend/modules/user/controllers/SignInController.php

--- backend/controllers/SignInController.php
+++ backend/controllers/SignInController.php
@@ -81,7 +81,7 @@ class SignInController extends Controller
     public function actionProfile()
     {
         $model = Yii::$app->user->identity->userProfile;
-        if ($model->load($_POST) && $model->save()) {
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
             Yii::$app->session->setFlash('alert', [
                 'options'=>['class'=>'alert-success'],
                 'body'=>Yii::t('backend', 'Your profile has been successfully saved', [], $model->locale)
@@ -97,7 +97,7 @@ class SignInController extends Controller
         $model = new AccountForm();
         $model->username = $user->username;
         $model->email = $user->email;
-        if ($model->load($_POST) && $model->validate()) {
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
             $user->username = $model->username;
             $user->email = $model->email;
             if ($model->password) {
