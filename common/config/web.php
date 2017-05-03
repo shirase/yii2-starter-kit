8af6de68059472c4e1a35f897fe68b6193bc7ff2
shirase <shirase55@gmail.com>
2017-05-03 17:40:46 +0300

    common/config/web.php

--- common/config/web.php
+++ common/config/web.php
@@ -60,7 +60,7 @@ if (YII_DEBUG) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
-        'allowedIPs' => ['127.0.0.1', '::1', '192.168.33.1', '172.17.42.1', '172.17.0.1', '192.168.99.1'],
+        'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 }
 
