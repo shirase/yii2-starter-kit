f726d1172c2e3258616f5942ee32d16c271c272e
shirase <shirase55@gmail.com>
2017-05-26 14:12:52 +0300

    bin/bundle.sh

--- bin/bundle.sh
+++ bin/bundle.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd ${0%/*}
 ../console/yii asset ../backend/config/assets/compress.php ../backend/config/assets/_bundles.php
 ../console/yii asset ../frontend/config/assets/compress.php ../frontend/config/assets/_bundles.php
