62fda4e85e1cb929be5b34a69a0237a228560401
shirase <shirase55@gmail.com>
2017-06-09 00:21:05 +0300

    `backend/models/LoginForm::getUser` check active

--- backend/models/LoginForm.php
+++ backend/models/LoginForm.php
@@ -90,6 +90,7 @@ class LoginForm extends Model
     {
         if ($this->user === false) {
             $this->user = User::find()
+                ->active()
                 ->andWhere(['or', ['username'=>$this->username], ['email'=>$this->username]])
                 ->one();
         }
