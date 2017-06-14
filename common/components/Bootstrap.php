e7abd7fc6138d81838c928fa46956b522d9ecdcd
shirase <shirase55@gmail.com>
2017-06-14 13:06:17 +0300

    Common bootstrap alias directory exists check

--- common/components/Bootstrap.php
+++ common/components/Bootstrap.php
@@ -6,7 +6,10 @@ use yii\base\BootstrapInterface;
 class Bootstrap implements BootstrapInterface
 {
     public function bootstrap($app) {
-        \Yii::setAlias('@bower', '@vendor/bower-asset');
-        \Yii::setAlias('@npm', '@vendor/npm-asset');
+        if (is_dir(\Yii::getAlias('@vendor/bower-asset')))
+            \Yii::setAlias('@bower', '@vendor/bower-asset');
+
+        if (is_dir(\Yii::getAlias('@vendor/npm-asset')))
+            \Yii::setAlias('@npm', '@vendor/npm-asset');
     }
 }
