a53f910bb17d367e9509b0051c3b4e08e5660b5a
shirase <shirase55@gmail.com>
2017-06-22 11:40:30 +0300

    Common bootstrap bower and npm aliases

--- common/components/Bootstrap.php
+++ common/components/Bootstrap.php
@@ -6,10 +6,7 @@ use yii\base\BootstrapInterface;
 class Bootstrap implements BootstrapInterface
 {
     public function bootstrap($app) {
-        if (is_dir(\Yii::getAlias('@vendor/bower-asset')))
-            \Yii::setAlias('@bower', '@vendor/bower-asset');
-
-        if (is_dir(\Yii::getAlias('@vendor/npm-asset')))
-            \Yii::setAlias('@npm', '@vendor/npm-asset');
+        \Yii::setAlias('@bower', '@vendor/bower-asset');
+        \Yii::setAlias('@npm', '@vendor/npm-asset');
     }
 }
