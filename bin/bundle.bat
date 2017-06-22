f5119c477b91e8cae86644d3d3b1ede339042747
shirase <shirase55@gmail.com>
2017-06-22 14:58:41 +0300

    bundle

--- bin/bundle.bat
+++ bin/bundle.bat
@@ -1,3 +1,2 @@
-cd "%~dp0"
-CALL ..\console\yii asset ..\backend\config\assets\compress.php ..\backend\config\assets\_bundles.php
-CALL ..\console\yii asset ..\frontend\config\assets\compress.php ..\frontend\config\assets\_bundles.php
+CALL %~dp0..\console\yii asset %~dp0..\backend\config\assets\compress.php %~dp0..\backend\config\assets\_bundles.php
+CALL %~dp0..\console\yii asset %~dp0..\frontend\config\assets\compress.php %~dp0..\frontend\config\assets\_bundles.php
