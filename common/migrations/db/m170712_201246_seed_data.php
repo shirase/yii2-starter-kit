f5d0dca6246333a67f45b8148e0fca9b8e05451c
shirase <shirase55@gmail.com>
2017-07-12 23:17:29 +0300

    common/migrations/db/m170712_201246_seed_data.php fix

--- common/migrations/db/m170712_201246_seed_data.php
+++ common/migrations/db/m170712_201246_seed_data.php
@@ -13,7 +13,7 @@ class m170712_201246_seed_data extends Migration
             'password_hash' => Yii::$app->getSecurity()->generatePasswordHash('webmaster'),
             'auth_key' => Yii::$app->getSecurity()->generateRandomString(),
             'access_token' => Yii::$app->getSecurity()->generateRandomString(40),
-            'status' => User::STATUS_ACTIVE,
+            'status' => \common\models\User::STATUS_ACTIVE,
             'created_at' => time(),
             'updated_at' => time()
         ]);
