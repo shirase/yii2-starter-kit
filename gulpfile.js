12748f62cdab2ca380eb4e0ed56115b679f8c332
shirase <shirase55@gmail.com>
2017-08-10 13:30:45 +0300

    Gulp rollup ignore jquery

--- gulpfile.js
+++ gulpfile.js
@@ -155,7 +155,9 @@ gulp.task('js-frontend', function() {
                         moduleDirectory: ['node_modules', 'vendor/npm-asset', 'vendor/bower-asset']
                     }
                 }),
-                commonjs(),
+                commonjs({
+                    ignore: ['jquery']
+                }),
                 rollupBabel({
                     babelrc: false,
                     presets: [
