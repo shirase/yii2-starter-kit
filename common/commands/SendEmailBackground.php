83caabf9095083b241beafad59b312a00fdb1260
shirase <shirase55@gmail.com>
2017-08-29 18:28:29 +0300

    `common/commands/SendEmailBackground`

---
+++ common/commands/SendEmailBackground.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace common\commands;
+
+use trntv\bus\interfaces\BackgroundCommand;
+use trntv\bus\middlewares\BackgroundCommandTrait;
+use yii\base\Object;
+use yii\swiftmailer\Message;
+use trntv\bus\interfaces\SelfHandlingCommand;
+
+class SendEmailBackground extends SendEmailCommand implements SelfHandlingCommand, BackgroundCommand
+{
+    use BackgroundCommandTrait;
+
+    public $appPath;
+
+    public function init()
+    {
+        parent::init();
+        $this->appPath = \Yii::getAlias('@app');
+    }
+
+    /**
+     * @param SendEmailBackground $command
+     */
+    public function handle($command) {
+        \Yii::setAlias('@app', $command->appPath);
+        qwe();
+        return parent::handle($command);
+    }
+}
