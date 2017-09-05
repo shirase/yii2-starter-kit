20a03877117cfdd1ab665a79d99c321ec5125654
shirase <shirase55@gmail.com>
2017-09-05 12:12:41 +0300

    `common/components/Bootstrap` set `forcePageParam` as false for `yii\data\Pagination`

--- common/components/Bootstrap.php
+++ common/components/Bootstrap.php
@@ -2,10 +2,13 @@
 namespace common\components;
 
 use yii\base\BootstrapInterface;
+use yii\data\Pagination;
 
 class Bootstrap implements BootstrapInterface
 {
     public function bootstrap($app) {
-        
+        \Yii::$container->set(Pagination::class, [
+            'forcePageParam' => false,
+        ]);
     }
 }
