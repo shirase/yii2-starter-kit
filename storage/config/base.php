f79f6c057daf96b2daa152f07c01284131280e40
shirase <shirase55@gmail.com>
2017-09-26 00:21:53 +0300

    Glide jpeg default quality 80

--- storage/config/base.php
+++ storage/config/base.php
@@ -16,7 +16,10 @@ return [
             'sourcePath' => '@storage/web/source',
             'cachePath' => '@storage/web/cache',
             'maxImageSize' => env('GLIDE_MAX_IMAGE_SIZE'),
-            'signKey' => env('GLIDE_SIGN_KEY')
+            'signKey' => env('GLIDE_SIGN_KEY'),
+            'defaults' => [
+                'q' => 80,
+            ],
         ],
         'request' => [
             'baseUrl' => rtrim(Yii::getAlias('@storageUrl'), '/'),
