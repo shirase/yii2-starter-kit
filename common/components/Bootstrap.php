7a0f8d360983ed4ba6c6b1943d997a2ce817499f
shirase <shirase55@gmail.com>
2017-10-03 15:34:27 +0300

    jquery min by default

--- common/components/Bootstrap.php
+++ common/components/Bootstrap.php
@@ -3,12 +3,19 @@ namespace common\components;
 
 use yii\base\BootstrapInterface;
 use yii\data\Pagination;
+use yii\web\JqueryAsset;
 
 class Bootstrap implements BootstrapInterface
 {
     public function bootstrap($app) {
         \Yii::$container->set(Pagination::class, [
             'forcePageParam' => false,
+        ]);
+
+        \Yii::$container->set(JqueryAsset::class, [
+            'js' => [
+                'jquery.min.js',
+            ]
         ]);
     }
 }
