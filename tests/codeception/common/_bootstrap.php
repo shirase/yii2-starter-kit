44471bf56d36e8b8ca67a5ccfa5bf3456f24b850
shirase <shirase55@gmail.com>
2017-12-21 01:08:44 +0300

    tests remove common $_SERVER patch

--- tests/codeception/common/_bootstrap.php
+++ tests/codeception/common/_bootstrap.php
@@ -10,10 +10,6 @@ defined('YII_APP_BASE_PATH') or define('YII_APP_BASE_PATH', dirname(dirname(dirn
 require_once(YII_APP_BASE_PATH . '/vendor/yiisoft/yii2/Yii.php');
 require_once(YII_APP_BASE_PATH . '/common/config/bootstrap.php');
 
-// set correct script paths
-$_SERVER['SERVER_NAME'] = 'localhost';
-$_SERVER['SERVER_PORT'] = '8080';
-
 $config = require(__DIR__ . '/../config/common/unit.php');
 new yii\web\Application($config);
 
