6890d922f61f7ada73a15d0d64fd715d6b90f4df
shirase <shirase55@gmail.com>
2017-12-21 00:08:35 +0300

    tests/codeception/backend/_pages/LoginPage.php

--- tests/codeception/backend/_pages/LoginPage.php
+++ tests/codeception/backend/_pages/LoginPage.php
@@ -1,59 +1,33 @@
 <?php
-
 namespace tests\codeception\backend\_pages;
-use yii\base\Component;
-use yii\base\InvalidConfigException;
-
+use tests\codeception\backend\AcceptanceTester;
+use yii\helpers\Url;
 /**
- * Represents loging page
+ * Represents login page
  */
-class LoginPage extends Component
+class LoginPage
 {
-    /**
-     * @var \Codeception\Actor the testing guy object
-     */
+    /** @var string */
+    public $route = '/sign-in/login';
+    /** @var AcceptanceTester */
     protected $actor;
-
-    public function __construct($I)
-    {
-        $this->actor = $I;
-    }
-
     /**
-     * Returns the URL to this page.
-     * The URL will be returned by calling the URL manager of the application
-     * with [[route]] and the provided parameters.
-     * @param array $params the GET parameters for creating the URL
-     * @return string the URL to this page
-     * @throws InvalidConfigException if [[route]] is not set or invalid
+     * LoginPage constructor.
+     * @param $actor
      */
-    public function getUrl($params = [])
+    public function __construct($actor)
     {
-        if (is_string($this->route)) {
-            $params[0] = $this->route;
-            return \Yii::$app->getUrlManager()->createUrl($params);
-        } elseif (is_array($this->route) && isset($this->route[0])) {
-            return \Yii::$app->getUrlManager()->createUrl(array_merge($this->route, $params));
-        } else {
-            throw new InvalidConfigException('The "route" property must be set.');
-        }
+        $this->actor = $actor;
+        $this->actor->amOnPage(Url::to($this->route));
     }
-
     /**
-     * Creates a page instance and sets the test guy to use [[url]].
-     * @param \Codeception\Actor $I the test guy instance
-     * @param array $params the GET parameters to be used to generate [[url]]
-     * @return static the page instance
+     * @param $actor
+     * @return LoginPage
      */
-    public static function openBy($I, $params = [])
+    public static function openBy($actor)
     {
-        $page = new static($I);
-        $I->amOnPage($page->getUrl($params));
-        return $page;
+        return new self($actor);
     }
-
-    public $route = 'sign-in/login';
-
     /**
      * @param string $username
      * @param string $password
