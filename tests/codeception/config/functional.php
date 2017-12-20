b6b4439bf394744e8c47134ffb265139c50e6991
shirase <shirase55@gmail.com>
2017-12-21 02:20:30 +0300

    tests reset baseUrl for functional

--- tests/codeception/config/functional.php
+++ tests/codeception/config/functional.php
@@ -5,6 +5,8 @@
 return [
     'components' => [
         'request' => [
+            'baseUrl' => null,
+
             // it's not recommended to run functional tests with CSRF validation enabled
             'enableCsrfValidation' => false,
             // but if you absolutely need it set cookie domain to localhost
