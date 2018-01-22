93df8b39a1359abbcca543624667465e6b2c16ec
shirase <shirase55@gmail.com>
2018-01-22 23:52:24 +0300

    frontendCache

--- frontend/config/base.php
+++ frontend/config/base.php
@@ -4,7 +4,7 @@ return [
     'basePath' => dirname(__DIR__),
     'components' => [
         'urlManager' => require(__DIR__.'/_urlManager.php'),
-        'cache' => require(__DIR__.'/_cache.php'),
+        'frontendCache' => require(__DIR__.'/_cache.php'),
     ],
     /*'as locale' => [
         'class' => 'common\behaviors\LocaleBehavior',
