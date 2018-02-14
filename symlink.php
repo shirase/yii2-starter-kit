50f677faf83a83d39496b41ee89092047ebdbcc9
shirase <shirase55@gmail.com>
2018-02-14 23:21:24 +0300

    symlink.php

--- symlink.php
+++ symlink.php
@@ -1,12 +1,12 @@
 <?php
 exec('ln -s ../app/frontend/web/assets assets');
-exec('ln -s ../app/frontend/web/bundle assets');
-exec('ln -s ../app/frontend/web/ckeditor assets');
-exec('ln -s ../app/frontend/web/css assets');
-exec('ln -s ../app/frontend/web/data assets');
-exec('ln -s ../app/frontend/web/img assets');
-exec('ln -s ../app/frontend/web/js assets');
-exec('ln -s ../app/frontend/web/svg assets');
+exec('ln -s ../app/frontend/web/bundle bundle');
+exec('ln -s ../app/frontend/web/ckeditor ckeditor');
+exec('ln -s ../app/frontend/web/css css');
+exec('ln -s ../app/frontend/web/data data');
+exec('ln -s ../app/frontend/web/img img');
+exec('ln -s ../app/frontend/web/js js');
+exec('ln -s ../app/frontend/web/svg svg');
 
 exec('ln -s ../app/backend/web admin');
 exec('ln -s ../app/storage/web storage');
