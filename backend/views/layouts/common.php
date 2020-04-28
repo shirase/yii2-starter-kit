a04d1c041130c0c5793d5d5e04fbcbf4d553ef61
Andrey Akimov <shirase55@gmail.com>
2020-04-28 23:47:24 +0300

    sidebar-menu tree

--- backend/views/layouts/common.php
+++ backend/views/layouts/common.php
@@ -145,7 +145,7 @@ $bundle = BackendAsset::register($this);
                     }
                 ?>
                 <?php echo Menu::widget([
-                    'options'=>['class'=>'sidebar-menu'],
+                    'options' => ['class' => 'sidebar-menu tree', 'data' => ['widget' => 'tree']],
                     'linkTemplate' => '<a href="{url}">{icon}<span>{label}</span>{right-icon}{badge}</a>',
                     'submenuTemplate'=>"\n<ul class=\"treeview-menu\">\n{items}\n</ul>\n",
                     'activateParents'=>true,
